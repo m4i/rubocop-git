5c3a421faad05d1241f79bec5b5f1ee614360799
Nick Campbell <nickcampbell18@gmail.com>
2014-10-16 13:19:53 +0100

    Program should exit with nonzero code for scripting
    1 is better than zero.

--- lib/rubocop/git/runner.rb
+++ lib/rubocop/git/runner.rb
@@ -11,6 +11,8 @@ module RuboCop
         @files = DiffParser.parse(git_diff(options))
 
         display_violations($stdout)
+        
+        exit(1) if violations.any?
       end
 
       private
