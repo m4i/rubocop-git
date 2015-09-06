7aa86d5aba31325adf75583e1e82b4d8bf7a538b
Laurent Arnoud <laurent@spkdev.net>
2015-09-06 14:17:03 +0200

    Add exit with violations for RuboCop::Git::Runner

--- test/rubocop/git/runner_test.rb
+++ test/rubocop/git/runner_test.rb
@@ -2,7 +2,18 @@ require_relative '../../test_helper'
 require 'rubocop/git/runner'
 
 describe RuboCop::Git::Runner do
-  it 'fail with invalid options' do
+  it 'exit with violations' do
+    options = RuboCop::Git::Options.new
+    # lib/rubocop/git/runner.rb:14:1: C: Trailing whitespace detected.
+    options.commits = ["v0.0.4", "v0.0.5"]
+    proc do
+      _out, _err = capture_io do
+        RuboCop::Git::Runner.new.run(options)
+      end
+    end.must_raise(SystemExit)
+  end
+
+  it 'fail with no options' do
     proc do
       _out, _err = capture_io do
         RuboCop::Git::Runner.new.run({})
